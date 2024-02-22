6d51b1523a635a42c62317ca945edb811b8ab563
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2024-02-22 23:49:20 +0700

    feat: add long mode test

--- loader.asm
+++ loader.asm
@@ -2,6 +2,20 @@
 [ORG 0x7e00]
 
 start:
+    mov [DriveId],dl
+
+    mov eax,0x80000000
+    cpuid
+    cmp eax,0x80000001
+    jb NotSupport
+
+    mov eax,0x80000001
+    cpuid
+    test edx,(1<<29)
+    jz NotSupport
+    test edx,(1<<26)
+    jz NotSupport
+
     mov ah,0x13
     mov al,1
     mov bx,0xa
@@ -10,10 +24,11 @@ start:
     mov cx,MessageLen 
     int 0x10
 
+NotSupport:
 End:
     hlt
     jmp End
 
-
-Message:    db "Loader starts"
+DriveId:    db 0
+Message:    db "long mode is supported"
 MessageLen: equ $-Message

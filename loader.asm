bd84844d883245b0a9331e87ae2901b0bb4b9109
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2024-02-23 20:58:27 +0700

    feat: add load kernel

--- loader.asm
+++ loader.asm
@@ -16,6 +16,19 @@ start:
     test edx,(1<<26)
     jz NotSupport
 
+LoadKernel:
+    mov si,ReadPacket
+    mov word[si],0x10
+    mov word[si+2],100
+    mov word[si+4],0
+    mov word[si+6],0x1000
+    mov dword[si+8],6
+    mov dword[si+0xc],0
+    mov dl,[DriveId]
+    mov ah,0x42
+    int 0x13
+    jc  ReadError
+
     mov ah,0x13
     mov al,1
     mov bx,0xa
@@ -24,11 +37,13 @@ start:
     mov cx,MessageLen 
     int 0x10
 
+ReadError:
 NotSupport:
 End:
     hlt
     jmp End
 
 DriveId:    db 0
-Message:    db "long mode is supported"
+Message:    db "kernel is loaded"
 MessageLen: equ $-Message
+ReadPacket: times 16 db 0
